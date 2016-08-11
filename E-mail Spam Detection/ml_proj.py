bd0978489d8e702ed57c111d31df7fc8184c86f7
Jvalin Dave <jvalin@Jvalins-MacBook-Air.local>
2016-08-11 16:01:51 -0500

    Spam detection code
    Python Program

--- E-mail Spam Detection/ml_proj.py
+++ E-mail Spam Detection/ml_proj.py
@@ -1,7 +1,9 @@
 
 # coding: utf-8
 
-# In[11]:
+# In[7]:
+
+#Naive Bayes on continuous attributes
 
 import csv
 import math
@@ -9,105 +11,159 @@ import numpy as np
 import collections
 import heapq
 from collections import Counter
-import statistics
-from statistics import mean
 
 
-meany = []
-meann =[]
-vary = []
-varn = []
+from random import randint
+import random
 
-print("\nTraining Files \t Test files \t Ratio")
-print("\n train_spam \t spam_test \t 95-5 \n Train(50) \t Test(50) \t 50-50\n train2 \t Test2 \t\t 40-60")
-nm = raw_input('\nEnter Training DataSet Name: ') 
-name = nm + '.txt'
+#list declarations for getting mean and variance of each column
 
-h = 57
-for i in range(h):
-    dy = []
-    dn = []    
-    with open(name, 'rb') as csv_file:
+mean_spam = []
+mean_non_spam =[]
+var_spam = []
+var_non_spam = []
+
+
+#taking base file in matrix
+
+fileload = np.loadtxt('spambase.txt')
+
+#gettin number of columns of the file
+
+cols = fileload[1]
+num_cols = len(cols)
+
+#getting split percentage from user
+
+split_percentage = raw_input('\nEnter Split Percentage for Training Data: ') 
+split_ratio = int(split_percentage)
+len_rows = len(fileload)
+
+#value of split percentage
+
+prcnt_to_num = int((split_ratio * len_rows)/100)
+
+#getting random unbiased unique rows for training data
+#t = training_range
+training_range = random.sample(range(0, len_rows), prcnt_to_num)
+
+
+#making two files for train and test
+#file_1 = open('train.txt', 'w+')
+#file_2 = open('test.txt','w+')
+    
+#splitting rows in each of the file for training and testing
+for i in range(len(fileload)):
+    if i in t:
+        with open('train.txt', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter='\t')
+            spamwriter.writerow(hp[i])
+    else:
+        with open('test.txt', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter='\t')
+            spamwriter.writerow(hp[i])
+        
+#in files the index start from 0 so subtracting 1 from total columns
+cols_to_read = num_cols-1
+resultant_col = num_cols-1
+
+#for every column getting mean and variance based on the class value
+for i in range(cols_to_read):
+    spam = []
+    non_spam = []    
+    with open('train.txt', 'rb') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\t')
         for row in csv_reader:
-            if row[h] == '1':
-                dy.append(float(row[i]))
+            #if its a spam 
+            if row[resultant_col] == '1.0':
+                spam.append(float(row[i]))
             else:
-                dn.append(float(row[i]))
-        
-    meany.append(np.mean(dy))
-    meann.append(np.mean(dn))
-    vary.append(np.var(dy))
-    varn.append(np.var(dn))
+                #not a spam
+                non_spam.append(float(row[i]))
+    
+    #using numpy for mean of each class and each column
+    mean_spam.append(np.mean(spam))
+    mean_non_spam.append(np.mean(non_spam))
+    var_spam.append(np.var(spam))
+    var_non_spam.append(np.var(non_spam))
         
 
-        
-prio = float(len(dy))/float(len(dy)+len(dn))
+#calculating prior probability based on training data
+
+prio = float(len(spam))/float(len(spam)+len(non_spam))
 print("Prior Probability of being spam : %s" % prio)
+
+#goal[] to store the predicted values based on classifier
 goal =[]
+#check[] will have actual values
 check = []
 
 
-nm = raw_input('Enter Test DataSet Name: ') 
-name_test = nm + '.txt'
 
-with open(name_test, 'rb') as csv_file:
+with open('test.txt', 'rb') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter='\t')
     for row in csv_reader:
+        #to multiply independent probabilities of every column 
         ans1 = 1.0
         ans2 = 1.0
-        check.append(float(row[57]))
-        for i in range(h-1):
-            a = float(row[i])
+        #getting actual class values
+        check.append(float(row[h]))
+        #not taking the last column as its class value
+        #checking the probability for each row to be spam and to not to be spam
+        for i in range(resultant_col-1):
+            read_cell = float(row[i])
         
-            avy = vary[i]
-            amy = meany[i]
+            read_var_spam = var_spam[i]   
+            read_mean_spam = mean_spam[i]  
         
-            v1 = math.pow(avy,2)
-            va = 2*v1
-            d1 = math.pow((a-amy),2)
-            if d1 == 0 or v1 == 0:
-                ex1 = 0
+            square_var = math.pow(read_var_spam,2)
+            twice_sq_var = 2*square_var
+            deno = math.pow((read_cell - read_mean_spam),2)
+            
+            if deno == 0 or square_var == 0:
+                exponent1 = 0
             else:
-                ex1 = -(d1/v1)
-            e1 = math.pow(math.e,ex1)
-            if v1 == 0:
-                sdn1 = 0
-                pa = 0
+                exponent1 = -(deno/twice_sq_var)
+                
+            e1 = math.pow(math.e,exponent1)
+            if read_cell == 0:
+                sqrt_deno = 0
+                prob_spam = 0
             else:
-                dn1 = 2*(math.pi)*v1
-                sdn1 = math.sqrt(dn1)
-                pa = float(e1)/float(sdn1)
+                deno_non_spam = 2*(math.pi)*v1
+                sqrt_deno = math.sqrt(deno_non_spam)
+                prob_spam = float(e1)/float(sqrt_deno)
 
         
-            avn = varn[i]
-            amn = meann[i]
-            v2 = math.pow(avn,2)
-            vb = 2*v2
-            d2 = math.pow((a-amn),2)
-            if v2 == 0:
-                ex2 = 0
-            else:
-                ex2 = -(d2/v2)
+            avar_non_spam = varn[i]
+            amean_non_spam = meann[i]
+            non_spam_val = math.pow(avar_non_spam,2)
             
-            e2 = math.pow(math.e,ex2)
-            dn2 = 2*(math.pi)*v2
-            sdn2 = math.sqrt(dn2)
-            if sdn2 == 0:
-                pb = 0
+            vb = 2*non_spam_val
+            d2 = math.pow((read_cell-amean_non_spam),2)
+            
+            
+            if non_spam_val == 0:
+                exponent2 = 0
             else:
-                pb = float(e2)/float(sdn2)
+                exponent2 = -(d2/v2)
+            
+            e2 = math.pow(math.e,exponent2)
+            deno_n = 2*(math.pi)*v2
+            sqrt_deno2 = math.sqrt(deno_n)
+            if sqrt_deno2 == 0:
+                prob_non_spam = 0
+            else:
+                prob_non_spam = float(e2)/float(sqrt_deno2)
+            #ans2 has probability of non-spam so multiplying with (1-prior_probability)
+            ans2 = prob_non_spam * ans2 *(1-prio)
+            #ans1 has probability of spam so multiplying with prior probability
+            ans1 = prob_spam * ans1 *prio
         
-            ans2 = pb * ans2 *(1-prio)
-            ans1 = pa * ans1 *prio
-        
+        #checking higher probability and assigning corresponding value
         if ans1 > ans2:
-            #print("Ans1 : %s" %(ans1))
-            #print("Ans2 : %s" %(ans2))
             goal.append(1.0)
         else:
-            #print("Ans1 : %s" %(ans1))
-            #print("Ans2 : %s" %(ans2))
             goal.append(0.0)
         
         
@@ -115,39 +171,29 @@ with open(name_test, 'rb') as csv_file:
         
         
         
+#checking how many are correctly classified       
 cnt = 0        
 for i in range(len(check)):
-    if check[i] == goal[i]:
+    if (float(check[i]) == float(goal[i])):
         cnt = cnt + 1
         
 
 acc = (float(cnt)/len(check))*100
 print("\nAccuracy: %s" % acc)
-print("\nClass Count")
+
+
+print('\nTotal Rows: %s ' %(ln))
+print('Number of rows for Training: %s' %(prc))
+
+print("\nClass Count (0.0 - Non-Spam , 1.0 - Spam)")
+print("\nClassified Values: ")
 print(Counter(goal))
+print("Actual Values: ")
 print(Counter(check))
 
 
 
-#for name, count in Counter(goal).most_common(1):
-#            c4,v4 = name,count
 
-        
-#for name, count in Counter(check).most_common(1):
-#            c5,v5 = name,count
-        
-
-#TP = 
-#FP = 
-#TN = 
-#FN =
-        
-        
-#Precision = TP/(TP+FP)
-#Recall = TP/(TP+FN)
-
-#print("\nPrecision: %s" %(Precision))
-#print("Recall: %s" % (Recall))
 
 
 
